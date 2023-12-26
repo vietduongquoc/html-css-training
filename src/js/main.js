7a5ce66a84209a3d4b8eff77c123f43aa025ce1e
DuongQuocViet2002 <vietduonsoaica@gmail.com>
2023-12-26 09:59:35 +0700

    .

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  console.log('hello')
-  var x = document.getElementById("header-topnav");
-  if (x.className === "header-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "header-nav";
-  }
-}
